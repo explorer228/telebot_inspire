3df753d3ae631360faae44fd6dec8f606e79488c
explorer228 <38917953+explorer228@users.noreply.github.com>
2018-05-18 15:49:06 +0100

    added constants.py

--- constants.py
+++ constants.py
@@ -1,6 +1,11 @@
 token = "571274483:AAFhPoNeivu8cE_C9hH7z72b4U74M2dY5rQ"
 
-words = ['ABC to fulminate against 1, 2, 3',
-            'Every spectator is a plotter, if he tries to explain a word (to know!)']
+words = [ 'Every spectator is a plotter, if he tries to explain a word (to know!) (Hugo Ball)',
+         'Lacking aura,  at least scatter its emanations.(Henri Michaux)',
+         '...the vicinity of the twentieth year, generally prefers to abandon man to his lusterless fate. (Andre Breton)',
+         'Beloved imagination, what I most like in you is your unsparing quality. (Andre Breton)',
+         'The genome is only a mirror for the breadth or narrowness of human imagination. (Siddhartha Mukherjee)'
+         ]
+
 import random
 random_message = lambda: random.choice(words)
